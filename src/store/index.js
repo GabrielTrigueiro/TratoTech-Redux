c3d9921d78e85d8cf5e8c5489472773d7960e59e
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-07-29 16:36:05 -0300

    motando itens e paginas de categorias

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { v4 as uuid } from "uuid";
+
 import categoriasSlice from "./reducers/categorias";
+import itensSlice from "./reducers/itens";
 
 const store = configureStore({
   reducer: {
     categorias: categoriasSlice,
+    itens: itensSlice,
   },
 });
 
