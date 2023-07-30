38b25536793896ddb0ce9e3a6bd02adc8c885910
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-07-30 12:18:48 -0300

    reducer resertar carrinho

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,9 +1,11 @@
 import Header from "components/Header";
 import styles from "./Carrinho.module.scss";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Item from "components/Item";
+import { resetarCarrinho } from "store/reducers/carrinho";
 
 export default function Carrinho() {
+  const dispatch = useDispatch();
   const { carrinho, total } = useSelector((state) => {
     let total = 0;
     const carrinhoReduce = state.carrinho.reduce((itens, itemNoCarrinho) => {
@@ -33,6 +35,12 @@ export default function Carrinho() {
             Subtotal: <strong> R$ {total.toFixed(2)} </strong>
           </span>
         </div>
+        <button
+          onClick={() => dispatch(resetarCarrinho())}
+          className={styles.finalizar}
+        >
+          Finalizar Compra
+        </button>
       </div>
     </div>
   );
--- src/store/reducers/carrinho.js
+++ src/store/reducers/carrinho.js
@@ -19,9 +19,11 @@ const carrinhoSlice = createSlice({
         return itemNoCarrinho;
       });
     },
+    resetarCarrinho: () => initialState,
   },
 });
 
-export const { mudarCarrinho, mudarQuantidade } = carrinhoSlice.actions;
+export const { mudarCarrinho, mudarQuantidade, resetarCarrinho } =
+  carrinhoSlice.actions;
 
 export default carrinhoSlice.reducer;
